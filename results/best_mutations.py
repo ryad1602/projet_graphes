2a7866fc5255c2ab08f668e8339b87c583f114d6
Ryad Messaoudi <117727201+ryad1602@users.noreply.github.com>
2026-05-14 19:29:11 +0200

    Update best_mutations.py

--- results/best_mutations.py
+++ results/best_mutations.py
@@ -1,4 +1,3 @@
-# Iteration 0 | cost=21.8s | taux=100%
 MUTATION_WEIGHTS = {
     # m_add_edge, m_rm_edge, m_add_node, m_rm_node, m_leaf, m_subdivide, m_path, m_clique, m_densify, m_contract, m_twins, m_complement_small
     'general': [0.7379, 1.8320, 1.8178, 0.1886, 0.7792, 1.2911, 0.6974, 1.6005, 1.4375, 0.9807, 1.7633, 1.2098],
